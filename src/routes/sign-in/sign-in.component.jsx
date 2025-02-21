c5080db3fae859b1b124b04e9ad8c74e22b59c5f
rhianeKobar <shakira.r.salazar@gmail.com>
2025-02-21 01:54:49 +0000

    refactor and style sign up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,23 @@
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithUserEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-	return(
-		<div>
-			<h1>sign in page</h1>
-		</div>
-	);
-}
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <div>
+      <h1>sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <SignUpForm />
+    </div>
+  );
+};
 
 export default SignIn;
